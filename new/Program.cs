d80c3c5639dab9c65e817b559bd1ad19f254fe60
pavel <dongod@bk.ru>
2022-12-29 00:13:36 +0400

    добавил игно

---
+++ new/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string[] array = { "Hello", "2", "world", ":-)" };
+int maxnum = 3;
+
+string[] FillArray(string[] array)
+{
+    
+    string[] result = new string[array.Length];
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxnum)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    foreach (string el in array)
+        Console.Write($"{el}  ");
+    Console.WriteLine();
+}
+
+string[] NewArray = FillArray(array);
+PrintArray(NewArray);
